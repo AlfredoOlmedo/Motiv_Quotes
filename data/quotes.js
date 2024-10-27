05a79f061bbef8c7546bcedaaee91b1374b4bb56
Alfredo Olmedo <ao@alfredoolmedo.com>
2024-10-27 15:47:05 -0300

    Modified Files

--- data/quotes.js
+++ data/quotes.js
@@ -1,13 +1,31 @@
-// Quotes
-let quote = document.getElementById('quote_dom');
-quote.innerHTML = 'Stoic Quote';
+// Obtener las frases desde el servidor FastAPI
+fetch('data/all_Phrases.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al obtener las frases');
+        }
+        return response.json(); // Convertimos la respuesta a JSON
+    })
+    .then(data => {
+        // Manipular el DOM para mostrar las frases
+        const quotesContainer = document.getElementById('containerQuotes');
+        quotesContainer.innerHTML = ''; // Limpiar el contenedor de frases
 
-let author = document.getElementById('author_dom');
-author.innerHTML = 'Zeneca';
+        data.forEach(quote => {
+            const quoteElement = document.createElement('div');
+            quoteElement.className = 'quote';
 
+            const quoteText = document.createElement('p');
+            quoteText.textContent = `"${quote.quote}"`;
 
+            const quoteAuthor = document.createElement('p');
+            quoteAuthor.textContent = `- ${quote.author}`;
 
-
-
-
-
+            quoteElement.appendChild(quoteText);
+            quoteElement.appendChild(quoteAuthor);
+            quotesContainer.appendChild(quoteElement);
+        });
+    })
+    .catch(error => {
+        console.error('Error al obtener las frases:', error);
+    });
--- stoic_api.py
+++ stoic_api.py
@@ -9,9 +9,9 @@ class quote(BaseModel):
     author: str
     
 # Read the JSON file and populate the quotes list    
-with open('data/all_Phrases.json', 'r') as file:
+with open('./data/all_Phrases.json', 'r') as file:
     quotes = json.load(file)
     
-@app.get("/")
+@app.get("data/all_Phrases.json")
 def read_quotes():
     return quotes
